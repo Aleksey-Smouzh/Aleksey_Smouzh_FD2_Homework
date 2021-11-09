cfaeda66f1a508084e68d18915962879bd103909
Aleksey-Smouzh <xmz@tut.by>
2021-11-09 11:04:50 +0300

    HomwWork№2(location changes)

---
+++ homeWork№2/homeWork2.js
@@ -0,0 +1,81 @@
+
+
+function arrNamberPlass() {
+  const items = [1, 2, 3];
+  for (let i = 0; i <= items.length; ++i) {
+      
+  }
+  items.shift();
+  items.push(4);
+
+  console.log(items);
+}
+arrNamberPlass();
+
+
+
+
+//   function changeCollection() {
+//       for (let i = 0; i < arguments.length -1; [i++]) {
+//          arguments[i];
+         
+//         }
+//         a.shift();
+//          a.push(4)
+//     }
+//     let a = [1, 2, 3];
+    
+//   changeCollection();
+  
+//     console.log(a);
+
+    // let arr = [1, 2, 3];
+    // for (let i = 0; i < arr.length; i++) {
+    //   arr.push;  
+    //    console.log( arr[i] );
+    //   }
+
+
+    // function changeCollection() {
+    //     for (let i = 0; i < arguments; i++) {
+    //       arguments[i];
+    //     }
+    //   }
+     
+    //   let a = [1, 2, 3];
+    //  a.shift();
+    //  a.push(4)
+    //   changeCollection(a);
+     
+    //   console.log(a);
+
+//  function array () {
+//         let arr = [1, 2, 3];
+//         for (let i = 0; i < arr.length -1; ++i) {
+//             arguments[i];
+//             arr.shift();
+//             arr.push(4);
+
+
+//             console.log(arr[i]);    
+//         } 
+        
+//      }
+//      array();  
+      
+// function  arrNamberPlass() {
+//     let arr = [1, 2, 3];
+
+//     arr.forEach(element => console.log(element));
+// }
+ 
+
+// function  arrNamberPlass() {}
+// const items = [1, 2, 3]
+//   for (let i = 0; i <= items.length; ++i) {
+     
+//   }  
+//   items.shift();
+//   items.push(4);
+
+// console.log(items)
